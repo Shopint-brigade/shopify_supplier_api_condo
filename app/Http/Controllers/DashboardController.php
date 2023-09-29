eccacaba005e33d70310ad0fb1b9164e070aacb0
fareskato <fareskato@gmail.com>
2023-09-29 20:52:54 +0300

    images

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -88,6 +88,7 @@ class DashboardController extends Controller
         $shopifyProductID = $request->input('product_id');
         // scrap product images from honey
         $honeyProductImages = HoneyUtils::parsingProductImages($honeyProductUrl);
+        info($honeyProductImages);
         // if we have images
         if (count($honeyProductImages) > 0) {
             // update shopify prtoduct images and get back the response status  
